a5e8350750f98afd14607f68c5e3384d06e4d61f
ITotalJustice <47043333+ITotalJustice@users.noreply.github.com>
2021-03-12 12:37:21 +0000

    add basic mmio r/w test for u8/u16/u32

--- src/mem/tests/test_constexpr_mem.cpp
+++ src/mem/tests/test_constexpr_mem.cpp
@@ -1,46 +1,31 @@
-// testing to see if it's possible to create a constexpr mem system
-// for the gba.
-// this likely requires a re-design of the arm struct,
-// so no longer treating the arm7tdmi as a seperate chip that gets
-// memory connected at runtime
-// instead, pass a memory interface struct as a template param!
-// this removes runtime errors / overhead and allows for constexpr stuff!
-
 #include "../mmio.hpp"
 
-#include <cstdint>
-#include <cassert>
-#include <array>
-// #include <vector> // not constexpr yet in gcc or clang...
-#include <span>
-#include <algorithm>
-
-namespace arm7tdmi {
-
-using u8 = std::uint8_t;
-using u16 = std::uint16_t;
-using u32 = std::uint32_t;
-using u64 = std::uint64_t;
-using s8 = std::int8_t;
-using s16 = std::int16_t;
-using s32 = std::int32_t;
-using s64 = std::int64_t;
-
-template <typename Mem>
-struct Gba {
-    
-    Mem mem{};
-};
+namespace gba::mem {
 
 constexpr auto compiletime_test() {
-    constexpr auto func = []() {
-        Gba<gba::mem::MMIO> Gba{};
-        Gba.mem.write<u32>(Gba.mem.IWRAM_REGION_START, 0xFF'00'69'FF);
-        const auto result = Gba.mem.read<u32>(Gba.mem.IWRAM_REGION_START);
-        return result == 0xFF'00'69'FF;
+    constexpr auto func = []<typename T>(T value) {
+        gba::mem::MMIO mem{};
+        
+        mem.write<T>(mem.IWRAM_REGION_START, value);
+        const auto result = mem.read<T>(mem.IWRAM_REGION_START);
+
+        return result == value;
     };
 
-    static_assert(func());
+    static_assert(
+        func.template operator()<u8>(0x12),
+        "failed basic mem u8 read/write test!"
+    );
+
+    static_assert(
+        func.template operator()<u16>(0x1234),
+        "failed basic mem u16 read/write test!"
+    );
+    
+    static_assert(
+        func.template operator()<u32>(0x12'34'56'78),
+        "failed basic mem u32 read/write test!"
+    );
 }
 
-} // namespace arm7tdmi
+} // namespace gba::mem
