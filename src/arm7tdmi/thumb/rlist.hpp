c6acc387251956eaa4db74692bdfc0fbd2801773
ITotalJustice <47043333+ITotalJustice@users.noreply.github.com>
2021-03-14 09:41:01 +0000

    make the rlist function more generic

--- src/arm7tdmi/thumb/rlist.hpp
+++ src/arm7tdmi/thumb/rlist.hpp
@@ -7,35 +7,50 @@
 
 namespace arm7tdmi::thumb {
 
+// todo: name these better
 enum class push_pop_type {
-    lr_0, // todo: name these better
-    lr_1,
+    lr_0, lr_1
 };
 
-constexpr auto rlist_callback(arm7tdmi& arm, const auto Callback, u32 addr, u32 rlist) {  
-    while (rlist != 0) {
-        const auto reg_index = std::countr_zero(rlist);
-        Callback(arm, addr, reg_index);
-        rlist = bit::unset(rlist, reg_index);
+enum class rlist_type {
+    store, load
+};
+
+// todo: clean this up, it's a little too over complicated atm
+template <rlist_type type>
+constexpr auto rlist_callback(arm7tdmi& arm, u32 addr, u32 rlist) {  
+    if constexpr(type == rlist_type::store) {
+        // the lowest reg is pushed to the lowest addr
+        // however it is still pushed first.
+        // because of this, calc the final offset and then write to it
+        // whilst incrementing the addr each time.
+        const auto final_addr = addr - (std::popcount(rlist) << 2);
+        addr = final_addr;
+
+        while (rlist != 0) {
+            const auto reg_index = std::countr_zero(rlist);
+            arm.mmio.write<u32>(
+                addr, arm.registers[reg_index]
+            );
+            addr += sizeof(u32);
+            rlist = bit::unset(rlist, reg_index);
+        }
+
+        return final_addr;
+    
+    } else {
+        while (rlist != 0) {
+            const auto reg_index = std::countr_zero(rlist);
+            arm.registers[reg_index] = arm.mmio.read<u32>(
+                addr
+            );
+            addr += sizeof(u32);
+            rlist = bit::unset(rlist, reg_index);
+        }
+
+        return addr;
     }
-
-    return addr;
 }
-
-constexpr auto push_callback(arm7tdmi& arm, u32& addr, const u32 reg_index) {
-    arm.mmio.write<u32>(
-        addr,
-        arm.registers[reg_index]
-    );
-    addr += sizeof(u32);
-};
-
-constexpr auto pop_callback(arm7tdmi& arm, u32& addr, u8 reg_index) {
-    arm.registers[reg_index] = arm.mmio.read<u32>(
-        addr
-    );
-    addr += sizeof(u32);
-};
 
 template <push_pop_type type>
 constexpr auto instruction_push(arm7tdmi& arm, const u32 rlist) {
@@ -46,15 +61,20 @@ constexpr auto instruction_push(arm7tdmi& arm, const u32 rlist) {
         if (rlist == 0) {
             assert(rlist != 0 && "not handled rlist == 0 edge case yet!");
         } else {
-            const auto addr = arm.get_sp() - (std::popcount(rlist) << 2);
-            rlist_callback(arm, push_callback, addr, rlist);
+            const auto addr = rlist_callback<rlist_type::store>(
+                arm,
+                arm.get_sp(),
+                rlist
+            );
             arm.set_sp(addr);
         }
     } else {
-        const auto new_rlist = rlist | 0b0111'1111'1111'1111;
-        const auto addr = arm.get_sp() - (std::popcount(new_rlist) << 2);
+        const auto addr = rlist_callback<rlist_type::store>(
+            arm, 
+            arm.get_sp(),
+            rlist | 0b0100'0000'0000'0000
+        );
         
-        rlist_callback(arm, push_callback, addr, new_rlist);
         arm.set_sp(addr);
     }
 }
@@ -66,16 +86,20 @@ constexpr auto instruction_pop(arm7tdmi& arm, const u8 rlist) {
         if (rlist == 0) {
             assert(rlist != 0 && "not handled rlist == 0 edge case yet!");
         } else {
-            const auto addr = arm.get_sp();
-            const auto new_addr = rlist_callback(arm, pop_callback, addr, rlist);
-            arm.set_sp(new_addr);
+            const auto addr = rlist_callback<rlist_type::load>(
+                arm,
+                arm.get_sp(),
+                rlist
+            );
+            arm.set_sp(addr);
         }
     } else {
-        const auto new_rlist = rlist | 0b1111'1111'1111'1111;
-        const auto addr = arm.get_sp();
-
-        const auto new_addr = rlist_callback(arm, pop_callback, addr, rlist);
-        arm.set_sp(new_addr);
+        const auto addr = rlist_callback<rlist_type::load>(
+            arm,
+            arm.get_sp(),
+            rlist | 0b1000'0000'0000'0000
+        );
+        arm.set_sp(addr);
     }
 }
 
@@ -84,8 +108,11 @@ constexpr auto instruction_stmia(arm7tdmi& arm, const u8 rb, const u8 rlist) {
     if (rlist == 0) {
         assert(rlist != 0 && "not handled rlist == 0 edge case yet!");
     } else {
-        const auto addr = arm.registers[rb] - (std::popcount(rlist) << 2);
-        rlist_callback(arm, push_callback, addr, rlist);
+        const auto addr = rlist_callback<rlist_type::store>(
+            arm,
+            arm.registers[rb],
+            rlist
+        );
         arm.registers[rb] = addr;
     }
 }
@@ -95,9 +122,12 @@ constexpr auto instruction_ldmia(arm7tdmi& arm, const u8 rb, const u8 rlist) {
     if (rlist == 0) {
         assert(rlist != 0 && "not handled rlist == 0 edge case yet!");
     } else {
-        const auto addr = arm.registers[rb];
-        const auto new_addr = rlist_callback(arm, pop_callback, addr, rlist);
-        arm.registers[rb] =  new_addr;
+        const auto addr = rlist_callback<rlist_type::load>(
+            arm,
+            arm.registers[rb],
+            rlist
+        );
+        arm.registers[rb] = addr;
     }
 }
 
